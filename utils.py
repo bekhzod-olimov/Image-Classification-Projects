9271aa863df11d7b9fc1da731372f50e089a11d7
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-07-06 11:42:04 +0900

    helper functions

--- utils.py
+++ utils.py
@@ -131,28 +131,50 @@ def visualize(all_ims, all_preds, all_gts, num_ims, rows, cls_names, save_path, 
     print(f"The visualization can be seen in {save_path} directory.")
     
 def grad_cam(model, all_ims, num_ims, rows, save_path, save_name):
+
+    """
+    
+    This function gets several parameters and visualizes GradCAM results.
+
+    Parameters:
+
+        model           - a trained model, torch model object;
+        all_ims         - images from the dataloader;
+        num_ims         - number of images to be visualized, int;
+        rows            - number of rows in the plot, int;
+        save_path       - path to save the visualization result, str;
+        save_name       - prefix of the name to be saved, str.
+    
+    """
     
     print("\nStart GradCAM visualization...")
+    # Set the figure size
     plt.figure(figsize = (20, 20))
+    # Get random indices from the dataloader based on the number of imags
     indices = [random.randint(0, len(all_ims) - 1) for _ in range(num_ims)]
     
+    # Go through random indices list
     for idx, ind in enumerate(indices):
+        # Get an image
         im = all_ims[ind]
+        # Convert the tensor image to numpy array
         ori_cam = tn2np((im)) / 255
+        # Initialize gradcam
         cam = GradCAM(model = model, target_layers = [model.features[-1]], use_cuda = True)
+        # Get grayscale gradcam result
         grayscale_cam = cam(input_tensor = im.unsqueeze(0))[0, :]
+        # Get combination of the input and gradcam result image
         vis = show_cam_on_image(ori_cam, grayscale_cam, image_weight = 0.6, use_rgb = True)
         
+        # Create a subplot
         plt.subplot(rows, num_ims // rows, idx + 1)
+        # Show the image
         plt.imshow(vis)
+        # Turn off the axis
         plt.axis("off")
+        # Set the title
         plt.title("GradCAM Visualization")
         
+    # Save the visualization result
     plt.savefig(f"{save_path}/{save_name}_gradcam.png")
     print(f"The GradCAM visualization can be seen in {save_path} directory.")
-    
-    
-    
-    
-    
-    
